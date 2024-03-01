8da1ddd689601cf177e08af79a5e27ba6898da65
benjiemc <benjiemc@telus.net>
2024-03-01 10:49:18 +0000

    feat: add molecular weights information

---
+++ src/python_pdb/chemistry.py
@@ -0,0 +1,8 @@
+MOLECULAR_WEIGHTS = {
+    'C': 12.011,
+    'N': 14.007,
+    'O': 15.999,
+    'S': 32.06,
+    'H': 1.008,
+}
+'''Molecular weights of protein atoms. Taken from https://www.rsc.org/periodic-table.'''
